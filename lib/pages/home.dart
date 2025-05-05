11295bf2b05ea3e08e999a309e0cfdf5c2eceb6a
fideline <fideline.bzn@yahoo.com>
2025-05-05 21:28:25 +0200

    search bar base

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xffff1D1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0
+                )
+              ]
+            ),
+            child: TextField( 
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding:  EdgeInsets.all(15), //reduce height of the input field
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none //remove borders
+                )
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
