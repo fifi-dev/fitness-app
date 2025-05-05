9215189495b3c5de551284cdfd1f6898c31dd3f3
fideline <fideline.bzn@yahoo.com>
2025-05-05 21:01:48 +0200

    adding action & clean code

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,18 +7,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Breakfast',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          ),
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Breakfast',
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold
         ),
-        //backgroundColor: Colors.white, //:seulement sur android
-        //elevation: 0.0, //enlever le shadow sur android
-        centerTitle: true,
-        leading: Container(
+      ),
+      //backgroundColor: Colors.white, //:seulement sur android
+      //elevation: 0.0, //enlever le shadow sur android
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          
+        },
+        child: Container(
           margin: EdgeInsets.all(10), //height & width will not work so margin of 10 all around
           alignment: Alignment.center, //fixes icon sizing
           child: SvgPicture.asset(
@@ -31,23 +40,28 @@ class HomePage extends StatelessWidget {
             borderRadius: BorderRadius.circular(10)
           ),
         ),
-        actions: [
-          Container(
-          margin: EdgeInsets.all(10), //height & width will not work so margin of 10 all around
-          alignment: Alignment.center, //fixes icon sizing
-          width: 37, //fixe right icon placement
-          child: SvgPicture.asset(
-            'assets/icons/dots.svg',
-            height: 5,
-            width: 5,
-          ),
-          decoration: BoxDecoration(
-            color: Color(0xffF7F9F8),
-            borderRadius: BorderRadius.circular(10)
-          ),
-        ),
-        ],
       ),
+      actions: [
+        GestureDetector(
+          onTap: () {
+            
+          },
+          child: Container(
+            margin: EdgeInsets.all(10), //height & width will not work so margin of 10 all around
+            alignment: Alignment.center, //fixes icon sizing
+            width: 37, //fixe right icon placement
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
+            ),
+            decoration: BoxDecoration(
+              color: Color(0xffF7F9F8),
+              borderRadius: BorderRadius.circular(10)
+            ),
+          ),
+        )
+      ],
     );
   }
 }
